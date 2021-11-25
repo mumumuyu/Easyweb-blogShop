df693b61d6add36b3375a9cb70ae30bc69c40137
lgd <1017276522@qq.com>
2021-11-25 21:03:43 +0800

    Easy version for download logo

--- src/main/java/com/lgd/controller/UploadController.java
+++ src/main/java/com/lgd/controller/UploadController.java
@@ -1,15 +1,13 @@
 package com.lgd.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.net.URLEncoder;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
@@ -43,7 +41,8 @@ public class UploadController {
     }
 
 
-    @PostMapping(value = "/upload", produces = "application/json;charset=UTF-8")
+//    @PostMapping(value = "/upload", produces = "application/json;charset=UTF-8")
+    @PostMapping("/upload")
     public HashMap singleFileUpload(@RequestParam("file") MultipartFile file, HttpServletRequest request) {
         HashMap hashMap = new HashMap();
         File targetFile=null;
@@ -96,5 +95,39 @@ public class UploadController {
         return hashMap;
     }
 
+    @GetMapping(value="/download")
+    public String downloads(HttpServletResponse response , HttpServletRequest request) throws Exception{
+        try {
+            //要下载的图片地址
+            String  path = "F:\\IDE\\Easyweb-boke\\src\\main\\resources\\static\\assets\\images\\logo.png";
+            String  fileName = "logo.png";
+            // path是指想要下载的文件的路径
+            File file = new File(path);
+
+            // 将文件写入输入流
+            FileInputStream fileInputStream = new FileInputStream(file);
+            InputStream fis = new BufferedInputStream(fileInputStream);
+            byte[] buffer = new byte[fis.available()];
+            fis.read(buffer);
+            fis.close();
+            // 清空response
+            response.reset();
+            // 设置response的Header
+            response.setCharacterEncoding("UTF-8");
+            //Content-Disposition的作用：告知浏览器以何种方式显示响应返回的文件，用浏览器打开还是以附件的形式下载到本地保存
+            //attachment表示以附件方式下载 inline表示在线打开 "Content-Disposition: inline; filename=文件名.mp3"
+            // filename表示文件的默认名称，因为网络传输只支持URL编码的相关支付，因此需要将文件名URL编码后进行传输,前端收到后需要反编码才能获取到真正的名称
+            response.addHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(fileName, "UTF-8"));
+            // 告知浏览器文件的大小
+            response.addHeader("Content-Length", "" + file.length());
+            OutputStream outputStream = new BufferedOutputStream(response.getOutputStream());
+            response.setContentType("application/octet-stream");
+            outputStream.write(buffer);
+            outputStream.flush();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
 
 }
